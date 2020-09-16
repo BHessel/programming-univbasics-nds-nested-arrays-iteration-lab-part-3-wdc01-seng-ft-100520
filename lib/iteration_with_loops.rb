c060e0a563a5616f1919f45627e0c83b2110ad09
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-16 01:11:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
     element_id = 0
     while element_id < src[array_count].count do
       if element_id.class == String
-        element_id << string_array
+        string_array << element_id
       # Then I shovel the element into the new array (string_array)
     end
     element_id += 1 
