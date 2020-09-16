4f34ae963328536492ebe7541a130a2fcfbd2611
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-16 01:05:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,21 @@
 def join_nested_strings(src)
-  # src will be an Array of Arrays of Strings and Integers
-  # Combine all Strings present in the AoA into a single value and return it
+  array_count = 0 
+  string_array = []
+  #is this right? Or should it be empty quotes?
+  while array_count < src.count do
+    element_id = 0
+    while element_id < src.count[array_count] do
+      if element_id.class == String
+        element_id << string_array
+      # Then I shovel the element into the new array (string_array)
+    end
+    element_id += 1 
+    #Here I keep scanning for more strings in this array
+  end
+  array_count += 1 
+  #and here I progress to the next array 
+end
+  string_array.join(' ')
+  #this failed
+  #here I want to take the array of strings and format it as one string 
 end
