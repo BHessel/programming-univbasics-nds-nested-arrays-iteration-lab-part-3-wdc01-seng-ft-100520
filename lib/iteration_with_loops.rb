c69a9c36913f1deff5e1fb4606d0bfd37294a99a
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-16 01:25:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def join_nested_strings(src)
   #is this right? Or should it be empty quotes?
   while array_count < src.count do
     element_id = 0
-    while element_id < src[array_count].count do
+    while element_id < src[array_count] do
       if src[array_count][element_id].class == String
         element_id << string_array
       # Then I shovel the element into the new array (string_array)
